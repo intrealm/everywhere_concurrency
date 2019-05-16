34e2d08881f85b231942537e78e7711d9344f76a
Sourav Pathak <soupatha@publicisgroupe.net>
2019-05-16 13:31:07 +0530

    gitignore setup

---
+++ src/test/java/conc/SampleTest.java
@@ -0,0 +1,13 @@
+package conc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void sampleRun()
+    {
+        Assert.assertNotSame(3,4);
+    }
+}
